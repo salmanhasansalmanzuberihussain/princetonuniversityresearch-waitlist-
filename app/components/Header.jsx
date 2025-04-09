82ad11da4273fafbc88555b0ac4a225319e5a8a6
m m <kmkl>
2025-04-09 17:33:00 -0400

    First Commit

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,27 +1,46 @@
-import Link from "next/link"
+import Link from 'next/link';
+import { UserButton, auth } from '@clerk/nextjs';
 
-const Header = () => {
-    return (
-        <>
-            <nav className="bg-blue-700 py-4 px-6 flex items-center justify-between mb-5">
-                <div className="flex items-center">
-                    <Link href="/">
-                        <div className='text-lg uppercase font-bold text-white'>
-                            Clerk App
-                        </div>
-                    </Link>
-                </div>
-                <div className='text-white'>
-                    <Link href='sign-in' className='text-gray-300 hover:text-white mr-4'>
-                        Sign In
-                    </Link>
-                    <Link href='sign-in' className='text-gray-300 hover:text-white mr-4'>
-                        Sign Up
-                    </Link>
-                </div>
-            </nav>
-        </>
-    )
-}
+const Header = async ({ username }) => {
+  const { userId } = auth();
+
+  return (
+    <nav className='flex items-center justify-between px-6 py-4 mb-5 bg-blue-700'>
+      <div className='flex items-center'>
+        <Link href='/'>
+          <div className='text-lg font-bold text-white uppercase'>
+            Clerk App
+          </div>
+        </Link>
+      </div>
+      <div className='flex items-center text-white'>
+        {!userId && (
+          <>
+            <Link
+              href='sign-in'
+              className='text-gray-300 hover:text-white mr-4'
+            >
+              Sign In
+            </Link>
+            <Link
+              href='sign-up'
+              className='text-gray-300 hover:text-white mr-4'
+            >
+              Sign Up
+            </Link>
+          </>
+        )}
+        {userId && (
+          <Link href='profile' className='text-gray-300 hover:text-white mr-4'>
+            Profile
+          </Link>
+        )}
+        <div className='ml-auto'>
+          <UserButton afterSignOutUrl='/' />
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Header;
--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,6 +1,9 @@
 const DashboardPage = () => {
     return (
-        <div>Dashboard Page</div>
+        <>
+            <h1 className="text-2xl font-fold mb-5">Dashboard</h1>
+            <p className="mb-5">Welcome to the Dashboard</p>
+        </>
     )
 }
 
